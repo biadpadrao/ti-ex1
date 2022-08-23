6becde24f158ef922f4ba52e22d5d9bc58b399f2
Beatriz Padrão <103266259+biadpadrao@users.noreply.github.com>
2022-08-23 11:58:38 -0300

    Create SomaDoisNum.java

---
+++ SomaDoisNum.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class SomaDoisNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1, num2, soma;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite um número");
+		num1 = sc.nextInt();
+		System.out.println("Digite outro número");
+		num2 = sc.nextInt();
+		
+		//somar
+		soma = num1 + num2;
+		
+		//mostrar na tela
+		System.out.println("Soma:" + soma);
+	}
+
+}
